346d0aa51ee46eafaf47e4d11a02966ae3064faa
Paulo Henrique Ziemer dos Santos <zs.phenriqueprofessional@gmail.com>
2024-02-22 11:03:56 -0300

    database configure method

--- Introduction/Blog/Data/BlogDataContext.cs
+++ Introduction/Blog/Data/BlogDataContext.cs
@@ -1,14 +1,20 @@
 namespace Blog.Data;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.SqlServer;
 using Microsoft.EntityFrameworkCore;
 
 // data context for the application with the Entity Framework
 public class BlogDataContext : DbContext{
     public DbSet<Category> Category { get; set; }
     public DbSet<Post> Post { get; set; }
-    public DbSet<PostTag> PostTag { get; set; }
+    // public DbSet<PostTag> PostTag { get; set; }
     public DbSet<Role> Role { get; set; }
     public DbSet<Tag> Tag { get; set; }
     public DbSet<User> User { get; set; }
-    public DbSet<UserRole> UserRole { get; set; }
+    // public DbSet<UserRole> UserRole { get; set; }
 
+    protected override void OnConfiguring
+    (DbContextOptionsBuilder options)
+        => options.UseSqlServer("Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$");
+    
 }
--- Introduction/Blog/Program.cs
+++ Introduction/Blog/Program.cs
@@ -3,5 +3,9 @@ using Blog.Data;
 
 class Program{
     static void main(string[] args){
+        using (var context = new BlogDataContext()){
+            var tag = new Tag { Name= "ASP.NET",Slug="aspnet" }
+            
+        }
     }
 }
